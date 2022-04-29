ddc073708dca8ede666b6f505c292f7434aac8dd
YGNice <yg@lianhe.art>
2022-04-29 18:09:36 +0800

    feat(locale): add zh-cn lang

--- src/locale/zh-CN.ts
+++ src/locale/zh-CN.ts
@@ -1,1 +1,50 @@
-export default {};
+export default {
+  'home': {
+    'coreProduct': '核心产品',
+    'digitalRightsManagementSystem': '数字版权管理系统',
+    'solveTheProblemOfCopyright': '解决数字版权管理、财务结算等痛点需求',
+    'connectCustomersWithValueInnovateAndAchieveCustomers': '将客户与价值连接起来、不断创新、成就客户',
+    'connectionValue': '连接价值',
+    'understandPeopleIsNeeds': '理解人们的需求',
+    'andThenConnectTheValueOfTheCustomer': '进而把虚拟星空的价值连接起来',
+    'continuousInnovate': '不断创新',
+    'sellInnovateNotProducts': '卖创新，而不是卖产品',
+    'createChangeYourself': '创造改变自己、影响市场的产品',
+    'achieveCustomers': '成就客户',
+    'weWantToHelpOurCustomersBecomeWhatTheyWant': '我们想帮我们的用户变成什么样',
+    'giveCustomersEnoughMotivationToImproveTheTeamValue': '给予客户足够的激励，提升团队价值',
+    'companyIntroduction': '公司介绍',
+    'companyIntroductionSubtitle': 'Company Introduction',
+    'companyIntroductionContent': '我们是一家专注提供音乐文化行业解决方案的软件开发公司，核心产品是一套数字版权管理 SaaS 系统。我们的客户包括中国的音乐人、词曲创作者、音乐文化公司，他们通过我们的系统为解决数字版权管理、财务结算等痛点需求。',
+    'companyIntroductionOurCustomers': '我们希望将我们的客户与价值连接起来、不断创新、成就客户。',
+    'productIntroduction': '产品介绍',
+    'productIntroductionSubtitle': 'Product Introduction',
+    'joinUs': '加入我们',
+    'ourTechnologyStack': '我们的技术栈是什么？',
+    'backendTechnologyStack': '后端: PHP / Python / Golang / Rust / Node.js',
+    'frontendTechnologyStack': '前端: TypeScript / React / Vue',
+    'ourEmployeesWelfare': '我们的员工福利',
+    'ourEmployeesWelfareContent': '基本的五险一金，上班时间是10:00-18:30，还有餐补车补',
+    'ourEmployeesWelfareContent2': '还有特别的员工关怀计划，包含生产力工具、健康、学习和提升等方面的福利',
+    'about': '关于',
+    'aboutUs': '关于我们',
+    'contactUs': '联系我们',
+    'policyAndPrivacy': '政策&隐私',
+    'privacyPolicy': '隐私政策',
+    'userAgreement': '用户协议',
+    'socialMedia': '社交媒体',
+    'enterpriseWeChat': '企业微信',
+    'aboutUsContent': '了解一下',
+    'application': {
+      'demoCloud': {
+        'desc': '词曲云依托区块链存证、电子签约和音频指纹等前沿技术，提供专业商务团队一对一助售服务，为创作者和音乐厂商提供一个安全、快捷、可靠的词曲买卖和授权平台。',
+      },
+      'cloudPush': {
+        'desc': '贯通头部UGC平台，立足短视频流量变现，实现数字音乐推广精准投放…',
+      },
+      'drm': {
+        'desc': '提供数字版权管理、财务结算等痛点需求的解决方案，让您的音乐产品更加安全、可靠、可信。',
+      },
+    }
+  }
+};
